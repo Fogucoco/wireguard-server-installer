a8dca1c1da0294812d536fbfa495c33fa6e7a138
Fogucoco <114810655+Fogucoco@users.noreply.github.com>
2024-12-12 22:42:29 +0300

    Add files via upload

--- add-wg-user.sh
+++ add-wg-user.sh
@@ -6,3 +6,56 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
+read -p "How many users to create? " user_number
+
+for ((i=0; i<user_number; i++))
+do
+    # checking user count
+    user_count=$(cat user-count)
+    user_folder="user${user_count}"
+    user_path="/etc/wireguard/user$user_count"
+
+    # getting server ip address
+    $ip_addr=$(curl ipinfo.io/ip)
+
+    # generating and reading keys
+    private_key=$(wg genkey)
+    public_key=$(echo $private_key | wg pubkey)
+    server_public_key=$(cat /etc/wireguard/server-keys/public.key)
+
+    # creating user folder and their config
+    mkdir $user_path
+    echo $private_key > $user_path/private.key
+    echo $public_key > $user_path/public.key
+
+    user_config="[Interface]
+PrivateKey = $private_key
+Address = 10.0.0.$user_count/32
+DNS = 8.8.8.8
+
+[Peer]
+PublicKey = $server_public_key
+Endpoint = ${ip_addr}:51820
+AllowedIPs = 0.0.0.0/0
+PersistentKeepalive = 20"
+
+    echo "$user_config" > $user_path/wg.conf
+
+    # adding new user to the server config
+    new_user_peer="
+[Peer]
+PublicKey = $public_key
+AllowedIPs = 10.0.0.$user_count/32"
+
+    echo "$new_user_peer" >> /etc/wireguard/wg0.conf
+done
+
+read -p "For new users to be enabled you must restart the service. All users will lose their connetion.
+Restart wireguard service? (Y/n): " answer
+answer=$(echo "$answer" | tr '[:upper:]' '[:lower:]')
+if [[ "$answer" == "y" || "$answer" == "" ]]; then
+    systemctl restart wg-quick@wg0
+else
+    echo "You can restart the service later by running:
+sudo systemctl restart wg-quick@wg0"
+fi
---
+++ install-wireguard.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+# root check
+if [ "$(id -u)" -ne 0 ]; then
+    echo "This script must be run as root " 
+    exit 1
+fi
+
+# updates
+apt update -y
+apt upgrade -y
+apt autoremove -y
+
+# ufw configuration
+ufw allow ssh
+ufw allow 51820
+ufw enable
+
+# installing dependancies
+apt install wget -y
+apt install curl -y
+
+# wireguard installation and configuration
+apt install wireguard -y
+echo -e "\nnet.ipv4.ip_forward=1" >> /etc/sysctl.conf
+sysctl -p
+
+chmod /etc/wireguard 700
+mkdir /etc/wireguard/server-keys
+
+private_key=$(wg genkey)
+public_key=$(echo $private_key | wg pubkey)
+echo "$private_key" > /etc/wireguard/server-keys/private.key
+echo "$public_key" > /etc/wireguard/server-keys/public.key
+
+wg0="[Interface]
+PrivateKey = $private_key
+Address = 10.0.0.1/24
+PostUp = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
+ListenPort = 51820
+"
+
+# vpn user creation
+echo 2 > /etc/wireguard/user-count
+
+wget https://raw.githubusercontent.com/Fogucoco/wireguard-server-installer/refs/heads/main/add-wg-user.sh
+chmod +x add-wg-user.sh
+
+./add-wg-user.sh
+
+wg-quick up wg0
